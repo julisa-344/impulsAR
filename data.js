3e06274e433a0ebaa9c92f086f8f8d62dc45b28f
Julisa Leon <julisa.leon344@gmail.com>
2023-12-28 08:53:33 -0500

    creando archivo data

---
+++ data.js
@@ -0,0 +1,69 @@
+// Objeto para almacenar la información de los modelos
+const modelos = [
+    {
+        id: "model1",
+        src: "https://cdn.glitch.me/33515771-c739-4b66-a7f1-88a11ff96e8c/prueba%20polar.glb?v=1703429063623",
+        iosSrc: "https://cdn.glitch.me/33515771-c739-4b66-a7f1-88a11ff96e8c/prueba%20polar.usdz?v=1703429031658",
+        poster: "imagen de carga",
+        alt: "texto alternativo",
+        shadowIntensity: 1,
+        cameraControls: true,
+        autoRotate: true,
+        ar: true
+    },
+    {
+        id: "model2",
+        src: "https://cdn.glitch.global/33515771-c739-4b66-a7f1-88a11ff96e8c/blanco.glb?v=1703356236274",
+        iosSrc: "https://cdn.glitch.me/33515771-c739-4b66-a7f1-88a11ff96e8c/blanco.usdz?v=1703356268392",
+        poster: "imagen de carga",
+        alt: "texto alternativo",
+        shadowIntensity: 1,
+        cameraControls: true,
+        autoRotate: true,
+        ar: true
+    },
+    {
+        id: "model3",
+        src: "https://cdn.glitch.global/33515771-c739-4b66-a7f1-88a11ff96e8c/negro.glb?v=1703301022722",
+        iosSrc: "https://cdn.glitch.me/33515771-c739-4b66-a7f1-88a11ff96e8c/negro.usdz?v=1703301462491",
+        poster: "imagen de carga",
+        alt: "texto alternativo",
+        shadowIntensity: 1,
+        cameraControls: true,
+        autoRotate: true,
+        ar: true
+    },
+    {
+        id: "model4",
+        src: "https://cdn.glitch.global/33515771-c739-4b66-a7f1-88a11ff96e8c/celeste.glb?v=1703356239422",
+        iosSrc: "https://cdn.glitch.me/33515771-c739-4b66-a7f1-88a11ff96e8c/celeste.usdz?v=1703356266855",
+        poster: "imagen de carga",
+        alt: "texto alternativo",
+        shadowIntensity: 1,
+        cameraControls: true,
+        autoRotate: true,
+        ar: true
+    },
+    {
+        id: "model5",
+        src: "https://cdn.glitch.global/33515771-c739-4b66-a7f1-88a11ff96e8c/blanco.glb?v=1703356236274",
+        iosSrc: "https://cdn.glitch.me/33515771-c739-4b66-a7f1-88a11ff96e8c/blanco.usdz?v=1703356268392",
+        poster: "imagen de carga",
+        alt: "texto alternativo",
+        shadowIntensity: 1,
+        cameraControls: true,
+        autoRotate: true,
+        ar: true
+    },
+    {
+        id: "model6",
+        src: "https://cdn.glitch.global/33515771-c739-4b66-a7f1-88a11ff96e8c/celeste.glb?v=1703356239422",
+        iosSrc: "https://cdn.glitch.me/33515771-c739-4b66-a7f1-88a11ff96e8c/celeste.usdz?v=1703356266855",
+        poster: "imagen de carga",
+        alt: "texto alternativo",
+        shadowIntensity: 1,
+        cameraControls: true,
+        autoRotate: true,
+        ar: true
+    }
+];
