4543a32dcb88882b9f51e1cef54fb3e41e3bcd6d
Julisa Leon <julisa.leon344@gmail.com>
2023-12-28 12:50:10 -0500

    formateando

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const modelos = require('./data.js');
+
 const carousel = document.getElementById("carousel");
 const modelContainer = carousel.querySelector(".model-container");
 const modelViewer = modelContainer.querySelector("model-viewer");
@@ -5,26 +7,18 @@ const modelViewer = modelContainer.querySelector("model-viewer");
 let currentModelIndex = 0;
 
 function prevModel() {
-  currentModelIndex = (currentModelIndex - 1 + modelos.length) % modelos.length;
-  updateModel();
-  if (currentModelIndex > 0) {
-    let imgElement = document.getElementById("prevImage");
-    imgElement.src = `img${currentModelIndex - 1}.png`;
-  }
+    currentModelIndex = (currentModelIndex - 1 + modelos.length) % modelos.length;
+    updateModelWithAnimationLeft();
 }
 
 function nextModel() {
-  currentModelIndex = (currentModelIndex + 1) % modelos.length;
-  updateModel();
-  if (currentModelIndex < modelos.length - 1) { // assuming totalModels is the total number of models
-    let imgElement = document.querySelector('.attribution .arrow-button img');
-    imgElement.src = `img${currentModelIndex}.png`;
-  }
+    currentModelIndex = (currentModelIndex + 1) % modelos.length;
+    updateModelWithAnimation();
 }
 
 function updateModel() {
-    let imgElementPrev = document.getElementById('prevImage');
-    let imgElementNext = document.getElementById('nextImage');
+    let imgElementPrev = document.getElementById("prevImage");
+    let imgElementNext = document.getElementById("nextImage");
     let prevIndex = currentModelIndex - 1;
     let nextIndex = currentModelIndex + 1;
 
@@ -38,19 +32,34 @@ function updateModel() {
 
     imgElementPrev.src = `assets/img${prevIndex + 1}.png`;
     imgElementNext.src = `assets/img${nextIndex + 1}.png`;
-  const currentModel = modelos[currentModelIndex];
-  modelViewer.setAttribute("src", currentModel.src);
-  modelViewer.setAttribute("ios-src", currentModel.iosSrc);
-  modelViewer.setAttribute("poster", currentModel.poster);
-  modelViewer.setAttribute("alt", currentModel.alt);
-  modelViewer.setAttribute("shadow-intensity", currentModel.shadowIntensity);
-  modelViewer.setAttribute("camera-controls", currentModel.cameraControls);
-  modelViewer.setAttribute("auto-rotate", currentModel.autoRotate);
-  modelViewer.setAttribute("ar", currentModel.ar);
-  modelContainer.style.animation = "transformX 0.5s ease-in-out";
-  setTimeout(() => {
-    modelContainer.style.animation = "";
-  }, 500);
+    const currentModel = modelos[currentModelIndex];
+    modelViewer.setAttribute("src", currentModel.src);
+    modelViewer.setAttribute("ios-src", currentModel.iosSrc);
+    modelViewer.setAttribute("poster", currentModel.poster);
+    modelViewer.setAttribute("alt", currentModel.alt);
+    modelViewer.setAttribute("shadow-intensity", currentModel.shadowIntensity);
+    modelViewer.setAttribute("camera-controls", currentModel.cameraControls);
+    modelViewer.setAttribute("auto-rotate", currentModel.autoRotate);
+    modelViewer.setAttribute("ar", currentModel.ar);
+}
+
+function updateModelWithAnimationLeft() {
+    modelContainer.style.transition = "transform 0.1s";
+    modelContainer.style.transform = "translateX(-100%)";
+    setTimeout(() => {
+        modelContainer.style.transition = "";
+        modelContainer.style.transform = "";
+        updateModel();
+    }, 100);
+}
+function updateModelWithAnimation() {
+    modelContainer.style.transition = "transform 0.1s";
+    modelContainer.style.transform = "translateX(100%)";
+    setTimeout(() => {
+        modelContainer.style.transition = "";
+        modelContainer.style.transform = "";
+        updateModel();
+    }, 100);
 }
 
 updateModel();
