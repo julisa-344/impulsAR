0e60eb7602dc541296f53cb6a9aaa3cd9e331241
Julisa Leon <julisa.leon344@gmail.com>
2023-12-28 11:49:16 -0500

    formateando

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-var modelContainers = document.querySelectorAll(".model-container");
-var currentIndex = 0;
+const carousel = document.getElementById("carousel");
+const modelContainer = carousel.querySelector(".model-container");
+const modelViewer = modelContainer.querySelector("model-viewer");
 
-// Añadir un evento para verificar si el modelo se ha cargado correctamente
-modelContainers.forEach(function (container, index) {
-    container
-        .querySelector("model-viewer")
-        .addEventListener("load", function () {
-            if (index === currentIndex) {
-                showModel(currentIndex);
-            }
-        });
-});
-
-function showModel(index) {
-    currentIndex = index;
-    modelContainers.forEach(function (container, i) {
-        container.style.transform = `translateX(${(i - index) * 100}%)`;
-    });
-}
+let currentModelIndex = 0;
 
 function prevModel() {
-    currentIndex = (currentIndex - 1 + modelContainers.length) % modelContainers.length;
-    showModel(currentIndex);
+  currentModelIndex = (currentModelIndex - 1 + modelos.length) % modelos.length;
+  updateModel();
+  if (currentModelIndex > 0) {
+    let imgElement = document.getElementById("prevImage");
+    imgElement.src = `img${currentModelIndex - 1}.png`;
+  }
 }
 
 function nextModel() {
-    currentIndex = (currentIndex + 1) % modelContainers.length;
-    showModel(currentIndex);
+  currentModelIndex = (currentModelIndex + 1) % modelos.length;
+  updateModel();
+  if (currentModelIndex < modelos.length - 1) { // assuming totalModels is the total number of models
+    let imgElement = document.querySelector('.attribution .arrow-button img');
+    imgElement.src = `img${currentModelIndex}.png`;
+  }
 }
 
-// Call showModel for the initial index
-showModel(currentIndex);
+function updateModel() {
+  const currentModel = modelos[currentModelIndex];
+  modelViewer.setAttribute("src", currentModel.src);
+  modelViewer.setAttribute("ios-src", currentModel.iosSrc);
+  modelViewer.setAttribute("poster", currentModel.poster);
+  modelViewer.setAttribute("alt", currentModel.alt);
+  modelViewer.setAttribute("shadow-intensity", currentModel.shadowIntensity);
+  modelViewer.setAttribute("camera-controls", currentModel.cameraControls);
+  modelViewer.setAttribute("auto-rotate", currentModel.autoRotate);
+  modelViewer.setAttribute("ar", currentModel.ar);
+  modelContainer.style.animation = "transformX 0.5s ease-in-out";
+  setTimeout(() => {
+    modelContainer.style.animation = "";
+  }, 500);
+}
+
+updateModel();
